8e77af318b505081ee4ff2167c813d1e16b75270
huylongdev <haolong2004crtt@gmail.com>
2024-06-10 17:18:29 +0700

    demo

---
+++ Sources/DOM/dom3-attri&text/dom3.js
@@ -0,0 +1,10 @@
+//Cách gán attribute
+//Cách 1 : setter
+var setter = document.querySelector("h1");
+setter.title = "vip pro"
+console.log(setter)
+
+
+var setter2 = document.querySelector("a");
+setter2.href = "fb.com"
+console.log(setter2)

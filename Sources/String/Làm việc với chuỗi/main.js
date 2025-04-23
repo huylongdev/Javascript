2881678ad0e134ebeb4e2faeaf85bc09bc5a2ab3
huylongdev <haolong2004crtt@gmail.com>
2025-04-23 12:59:31 +0700

    Hotfix-String-Split

--- Sources/String/Làm việc với chuỗi/main.js
+++ Sources/String/Làm việc với chuỗi/main.js
@@ -87,7 +87,7 @@ console.log("========================================");
 var languages = "JS, Java, Ruby";
 console.log(languages.split(", "));
 // The split() method is used to split a string into an array of substrings, and returns the new array.
-// ", " chính là điểm chung giữa các phần tử trong mảng mới, nếu không có thì nó sẽ cắt từng ký tự một như bên dưới.
+// ", " chính là điểm chung giữa các phần tử trong mảng , nếu không có thì nó sẽ cắt từng ký tự một như bên dưới.
 console.log(languages.split("")); // vì nó coi khoảng rỗng là điểm chung
 console.log("========================================");
 
